8d76604a9657be15564c853a0c12fa63b84cd8ee
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-26 02:10:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -12,4 +12,15 @@ function loadImages() {
     .then(results => {
       results.message.forEach(image => addImage(image))
     });
+} 
+
+function getBreedOptions() {
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
+
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
 }
